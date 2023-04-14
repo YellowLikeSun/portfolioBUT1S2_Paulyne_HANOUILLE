0529595e262e523ae674ff9a758a0f895ef6979b
YellowLikeSun <123458383+YellowLikeSun@users.noreply.github.com>
2023-04-14 11:51:24 +0200

    update config

--- Modele/config.php
+++ Modele/config.php
@@ -1,6 +1,8 @@
 <?php
-$host = 'localhost';
-$db = 'monportfolio';
-$user = 'root';
-$password = '';
+
+
+$host = 'eu-cdbr-west-03.cleardb.net';
+$db = 'heroku_fb79fa8d80c1335';
+$user = 'bba29db1b03601';
+$password = 'ad25a3fa';
 ?>
