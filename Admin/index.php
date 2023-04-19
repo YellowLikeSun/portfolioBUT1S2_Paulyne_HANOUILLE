2c2535b59063b2078ede56aacd8d6fb70df5b84b
YellowLikeSun <123458383+YellowLikeSun@users.noreply.github.com>
2023-04-19 16:33:48 +0200

    back office changes

--- Admin/index.php
+++ Admin/index.php
@@ -76,7 +76,7 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
+            <a href="#" class="nav-link">
               <i class="nav-icon"></i>
               <p>
                 Dashboard
