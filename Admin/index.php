bb64c27c92b53966790a3910014483005b63f844
YellowLikeSun <123458383+YellowLikeSun@users.noreply.github.com>
2023-04-19 17:26:20 +0200

    changes

--- Admin/index.php
+++ Admin/index.php
@@ -157,10 +157,10 @@
   </aside>
   <!-- /.control-sidebar -->
 </div>
-<!-- ./wrapper -->
+
 <!-- /.content-wrapper -->
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2014-2023 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.2.0
