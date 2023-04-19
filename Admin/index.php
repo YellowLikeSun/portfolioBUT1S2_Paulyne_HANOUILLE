53a1b994a6ad41c5fda6adf14d276b2173cc709b
YellowLikeSun <123458383+YellowLikeSun@users.noreply.github.com>
2023-04-19 16:46:44 +0200

    back officechanges

--- Admin/index.php
+++ Admin/index.php
@@ -69,7 +69,14 @@
           </div>
         </div>
       </div>
-
+      
+      <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    </ul>
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
