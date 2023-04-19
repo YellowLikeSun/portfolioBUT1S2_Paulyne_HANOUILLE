bd7496bac6d69d0178390511ab6c83db8ff63a48
YellowLikeSun <123458383+YellowLikeSun@users.noreply.github.com>
2023-04-19 16:53:59 +0200

    back office changes

--- Admin/index.php
+++ Admin/index.php
@@ -173,10 +173,10 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="../image/clementine.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="../Img/photositeweb.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">ClÃ©mentine Renault</a>
+          <a href="#" class="d-block">Paulyne HANOUILLE</a>
         </div>
       </div>
 
