3024e77e268f8a4b2028015e09ffb3ccff062e43
YellowLikeSun <123458383+YellowLikeSun@users.noreply.github.com>
2023-04-19 17:10:23 +0200

    back office change

--- Admin/index.php
+++ Admin/index.php
@@ -158,6 +158,14 @@
   <!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
+<!-- /.content-wrapper -->
+<footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
+  </footer>
 
 <!-- jQuery -->
 <script src="plugins/jquery/jquery.min.js"></script>
