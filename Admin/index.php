be9e9e5c60c205557fa93e75cdd41392cc20067d
YellowLikeSun <123458383+YellowLikeSun@users.noreply.github.com>
2023-04-19 16:44:42 +0200

    backoffice changes

--- Admin/index.php
+++ Admin/index.php
@@ -77,10 +77,10 @@
                with font-awesome or any other icon font library -->
           <li class="nav-item menu-open">
             <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <i class="nav-icon  fa-tachometer-alt"></i>
               <p>
                 Dashboard
-                <i class="right fas fa-angle-left"></i>
+                <i class="right "></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
