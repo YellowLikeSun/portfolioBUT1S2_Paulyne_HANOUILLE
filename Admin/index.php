6033010cc9b9e0adb556eea8068e143f6afefba9
YellowLikeSun <123458383+YellowLikeSun@users.noreply.github.com>
2023-05-01 22:35:45 +0200

    maj

--- Admin/index.php
+++ Admin/index.php
@@ -110,26 +110,9 @@ $user_data = $Query->fetchAll();
                 <i class="right"></i>
               </p>
             </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="./index.html" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v1</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./index2.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v2</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./index3.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v3</p>
-                </a>
-              </li>
-            </ul>
+            
+          
+         
           </li>
         </ul>
       </nav>
